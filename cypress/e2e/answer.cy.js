48811f6df663e3dccd05541fbe47420796fd4971
Vignesh Ganesaputarjunan <vignesh@vigneshs-mbp.home>
2022-06-15 14:56:14 +0100

    no comments

--- cypress/e2e/answer.cy.js
+++ cypress/e2e/answer.cy.js
@@ -4,9 +4,9 @@ describe('Answer Digital', function () {
    it('Test who we are page', function () {
       cy.visit('https://answerdigital.com')
       cy.title().should('include', 'Answer')
-      cy.get('span').contains('Who we are').click()
+      cy.get('span').contains('Who we are').should('be.visible').click({force:true})
       cy.contains('Proudly independent')
-      cy.get('span').contains('OUR ISO14001 POLICY').click()
+      cy.get('span').contains('OUR ISO14001 POLICY').should('be.visible').click({force:true})
 
 
    })
