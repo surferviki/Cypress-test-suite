60b5e74c923f77f21a9a60bc57c0840c88609dc1
Vignesh Ganesaputarjunan <vignesh@vigneshs-mbp.home>
2022-06-15 14:50:21 +0100

    Handling dom element

--- cypress/e2e/answer.cy.js
+++ cypress/e2e/answer.cy.js
@@ -1,25 +1,25 @@
 describe('Answer Digital', function () {
 
 
-    it('Test who we are page', function () { 
-       cy.visit('https://answerdigital.com')
-       cy.title().should('include', 'Answer')
-       cy.get('span').contains('Who we are').click()
-       cy.contains('Proudly independent')
-       cy.get('span').contains('OUR ISO14001 POLICY').click()
-       
-       
-    }) 
+   it('Test who we are page', function () {
+      cy.visit('https://answerdigital.com')
+      cy.title().should('include', 'Answer')
+      cy.get('span').contains('Who we are').click()
+      cy.contains('Proudly independent')
+      cy.get('span').contains('OUR ISO14001 POLICY').click()
 
 
-    it('Test Insights page', function () { 
-        cy.visit('https://answerdigital.com')
-           cy.get('span').contains('Insights').click()
-           cy.get('span').contains('Technology').click()
-           cy.contains('The NHS Needs AI Education').click()
-           cy.contains('Four pillars of education')
-           cy.contains('Quality control')
-    
-        }) 
+   })
 
-    })
+
+   it('Test Insights page', function () {
+      cy.visit('https://answerdigital.com')
+      cy.get('span').contains('Insights').should('be.visible').click({force:true})
+      cy.get('span').contains('Technology').should('be.visible').click({force:true})
+      cy.contains('The NHS Needs AI Education').should('be.visible').click({force:true})
+      cy.contains('Four pillars of education')
+      cy.contains('Quality control')
+
+   })
+
+})
